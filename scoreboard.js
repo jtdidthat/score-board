320ed4d16b5aef48b2313899d5dfde588089e701
jtdidthat <jtdidthatbusiness@gmail.com>
2022-10-19 17:08:50 -0700

    update

--- scoreboard.js
+++ scoreboard.js
@@ -1,37 +1,35 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
-let sumEl = document.getElementById("sum-el")
 
-// Create four functions: add(), subtract(), divide(), multiply()
-// Call the correct function when the user clicks on one of the buttons
-// Perform the given calculation using num1 and num2
-// Render the result of the calculation in the paragraph with id="sum-el"
+//home code
+let homeScore = 0
 
-// E.g. if the user clicks on the "Plus" button, you should render
-// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+document.getElementById("home-score").textContent = homeScore
+let HomeEl = document.getElementById("home-score")
 
-function add(){
-    let add = num1 + num2
-    sumEl.textContent = "Sum: "
-    sumEl.textContent += add
+function plus1Home(){
+    let plus1Home = homeScore + 1
+    homeScore = plus1Home 
+    HomeEl.textContent = plus1Home
+}
+    
+function plus2Home(){
+    let plus1Home = homeScore + 2
+    homeScore = plus1Home 
+    HomeEl.textContent = plus1Home
 }
 
-function subtract(){
-    let subtract = num1 - num2
-    sumEl.textContent = "Sum:"
-    sumEl.textContent += subtract
+function plus3Home(){
+    let plus1Home = homeScore + 3
+    homeScore = plus1Home 
+    HomeEl.textContent = plus1Home
 }
 
-function divide(){
-    let divide = num1 / num2
-    sumEl.textContent = "Sum:"
-    sumEl.textContent += divide
-}
 
-function multiply(){
-    let multiply = num1 * num2
-    sumEl.textContent = "Sum:"
-    sumEl.textContent += multiply
-}
+
+
+
+
+
+
+
+
+
