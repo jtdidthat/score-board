17b8fea2f0513c0776f6a99663420d5812d5c87f
jtdidthat <jtdidthatbusiness@gmail.com>
2022-10-20 16:57:20 -0700

    new

--- scoreboard.js
+++ scoreboard.js
@@ -3,33 +3,54 @@
 let homeScore = 0
 
 document.getElementById("home-score").textContent = homeScore
-let HomeEl = document.getElementById("home-score")
+let homeEl = document.getElementById("home-score")
 
 function plus1Home(){
     let plus1Home = homeScore + 1
     homeScore = plus1Home 
-    HomeEl.textContent = plus1Home
+    homeEl.textContent = plus1Home
 }
     
 function plus2Home(){
-    let plus1Home = homeScore + 2
-    homeScore = plus1Home 
-    HomeEl.textContent = plus1Home
+    let plus2Home = homeScore + 2
+    homeScore = plus2Home 
+    homeEl.textContent = plus2Home
 }
 
 function plus3Home(){
-    let plus1Home = homeScore + 3
-    homeScore = plus1Home 
-    HomeEl.textContent = plus1Home
+    let plus3Home = homeScore + 3
+    homeScore = plus3Home 
+    homeEl.textContent = plus3Home
 }
 
+//guest code 
 
+let guestScore = 0
 
+document.getElementById("guest-score").textContent = guestScore
+let guestEl = document.getElementById("guest-score")
 
+function plus1Guest(){
+    let plus1Guest = guestScore + 1
+    guestScore = plus1Guest 
+    guestEl.textContent = plus1Guest
+}
 
+function plus2Guest(){
+    let plus2Guest = guestScore + 2
+    guestScore = plus2Guest 
+    guestEl.textContent = plus2Guest
+}
 
+function plus3Guest(){
+    let plus3Guest = guestScore + 3
+    guestScore = plus3Guest 
+    guestEl.textContent = plus3Guest
+}
 
+//reset score
 
-
-
-
+function resetScore(){
+    guestEl.textContent = 0 
+    homeEl.textContent = 0
+}
